23eeb947ef6afffb09b318fe4b414e3bf9423349
Carver <kerenketepela@gmail.com>
2022-06-01 20:02:42 +0300

    feat:get all users

--- src/api/routes/admin/admin.router.js
+++ src/api/routes/admin/admin.router.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const {
   createNewUser,
+  getUsers,
 } = require("../../../controllers/admin/manageUsers.controller")
 const router = express.Router()
 
 router.route("/create").post(createNewUser)
+router.route("/").get(getUsers)
 
 module.exports = router
