65ed48145b62787b19f24ce5feae00584e324ae4
Carver <kerenketepela@gmail.com>
2022-06-01 20:16:34 +0300

    feat:init auth route

---
+++ src/api/routes/auth/auth.routes.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const { employeeLogin } = require("../../../controllers/auth/auth.employee")
+const router = express.Router()
+
+router.route("/login").post(employeeLogin)
+
+module.exports = router
