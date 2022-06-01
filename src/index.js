ae6f575d7722b6e638f79549de782b0ec77e89cf
Carver <kerenketepela@gmail.com>
2022-06-01 20:11:45 +0300

    feat:register route

--- src/index.js
+++ src/index.js
@@ -7,12 +7,16 @@ const app = express()
 
 // apis
 const adminRouter = require("./api/routes/admin/admin.router")
+const authRouter = require("./api/routes/auth/auth.routes")
+const articleRouter = require("./api/routes/articles/articles.routes")
 /*db init */
 require("./db/db.connect")
 
 app.use(express.json())
 
 /*api endpoints */
+app.use("/api/v1/articles/", articleRouter)
+app.use("/api/v1/users", authRouter)
 app.use("/api/v1/admin/users", adminRouter)
 
 const port = process.env.PORT
