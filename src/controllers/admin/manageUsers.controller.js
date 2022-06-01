5a0c0d62998afd1c8d4b1b163b6cb6809ff9df56
Carver <kerenketepela@gmail.com>
2022-06-01 20:09:44 +0300

    fix: check if user exists corrctly

--- src/controllers/admin/manageUsers.controller.js
+++ src/controllers/admin/manageUsers.controller.js
@@ -6,6 +6,11 @@ exports.createNewUser = async (req, res) => {
     if (!username || !email || !image || !password) {
       return res.status(400).json({ message: "all fields are required" })
     }
+    const savedUser = await User.findOne({ email: email })
+    //console.log(savedUser)
+    if (savedUser) {
+      return res.status(400).json({ message: "user already exists" })
+    }
     const newUser = await User.create({
       username,
       email,
@@ -15,17 +20,22 @@ exports.createNewUser = async (req, res) => {
     if (!newUser)
       return res.status(400).json({ message: "user is not created" })
     else {
-      await newUser.findOne({ email: req.params.email })
-      if (newUser) {
-        return res
-          .status(400)
-          .json({ message: "user already exists" })
-          .end()
-      }
       await newUser.save()
       res.status(200).json(newUser)
     }
   } catch (err) {
+    console.log(err)
     return res.status(400).json(err)
+  }
+}
+exports.getUsers = async (_req, res) => {
+  try {
+    const users = await User.find()
+    if (!users) {
+      res.status(400).json({ message: "no users found" })
+    }
+    res.status(200).json({ users })
+  } catch (err) {
+    res.status(400).end()
   }
 }
