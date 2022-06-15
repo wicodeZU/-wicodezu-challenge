e1f74d164d9d458535574bd3e208a6c057265fe8
Peter Tepela <kerenketepela@gmail.com>
2022-06-15 14:12:41 -0400

    feat:create git fn

--- src/controllers/gif/gifs.controller.js
+++ src/controllers/gif/gifs.controller.js
@@ -1,1 +1,38 @@
 // gifs crud ops
+/**
+ * Employees can create and share gifs
+ * They can delete gifs
+ * 
+ * common JS
+ * es2016
+ * 
+ * db
+ * mongo db
+ * 
+ * http verbs
+ * post
+ * get
+ * delete
+ * put
+ * patch
+ * 
+ * Error handling
+ */
+
+exports.createGif = async(req, res) =>{
+    const { gif } = req.body
+    try {
+        const doc = await Gif.create({
+            gif
+        })
+        if(!doc){
+            return res.status(401).json({message:"an error occured"})
+        }
+        await doc.save()
+        res.status(200).json({data:doc})
+        //
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json(error)
+    }
+}
